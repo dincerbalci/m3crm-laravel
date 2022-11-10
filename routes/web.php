47cfa11c839a666fc4b66ab4c06d739a6c95fe9b
Dawn36 <dawngill08@gmail.com>
2022-11-10 17:22:29 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,7 @@ use App\Http\Controllers\DashboardController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 // Route::get('/dashboard', function () {
