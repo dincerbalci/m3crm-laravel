fde6550be6efc4d33b22c8b156da34bafd6f713a
Dawn36 <dawngill08@gmail.com>
2022-12-07 16:52:05 +0500

    Update permission_error.blade.php

--- resources/views/error-page/permission_error.blade.php
+++ resources/views/error-page/permission_error.blade.php
@@ -35,9 +35,9 @@ License: You must have a valid license purchased only from themeforest(the above
                     <img alt="Midone - HTML Admin Template" class="h-48 lg:h-auto" src="{{ asset('theme/dist/images/error-illustration.svg')}}">
                 </div>
                 <div class="text-white mt-10 lg:mt-0">
-                    <div class="intro-x text-8xl font-medium">404</div>
-                    <div class="intro-x text-xl lg:text-3xl font-medium mt-5">Oops. This page has gone missing.</div>
-                    <div class="intro-x text-lg mt-3">You may have mistyped the address or the page may have moved.</div>
+                    <div class="intro-x text-8xl font-medium">400</div>
+                    <div class="intro-x text-xl lg:text-3xl font-medium mt-5">Oops. You do not have permission.</div>
+                    {{-- <div class="intro-x text-lg mt-3">Asked admin for.</div> --}}
                     <a href="{{route('dashboard')}}" class="intro-x btn py-3 px-4 text-white border-white dark:border-darkmode-400 dark:text-slate-200 mt-10">Back to Dashboard</a>
                 </div>
             </div>
