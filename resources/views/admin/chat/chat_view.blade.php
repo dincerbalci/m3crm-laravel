3fb4c9be18a5f90096eee0d8671b3b3bb5ab00f6
Dawn36 <dawngill08@gmail.com>
2023-01-25 17:24:46 +0500

    Update chat_view.blade.php

--- resources/views/admin/chat/chat_view.blade.php
+++ resources/views/admin/chat/chat_view.blade.php
@@ -4,6 +4,7 @@
     <link href="{{ asset('theme/dist/css/app.agent.css') }}" rel="stylesheet" type="text/css">
     <div class="chat-panel"> </div>
     @push('scripts')
+        <script src="{{ asset('theme/src/js/socket.io.js') }}" type="text/javascript"></script>
         <script src="https://helpdesk.m3tech.com.pk:3001/assets/core/js/app.agent.js" type="text/javascript"></script>
         <script>
             (function() {
